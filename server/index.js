b8f66f7a5ed641826721a6313a16a80c189ebb0a
Raymond Nhim <raymondnh@function-HotSoz.localdomain>
2022-04-09 01:58:16 -0700

    updated the input function, still needs work

--- server/index.js
+++ server/index.js
@@ -8,8 +8,10 @@ const path = require('path')
 // find the directory you want to serve, do not use /static or you'll have to use localhost:3000/static
 app.use('/', express.static(path.join(__dirname, '..', 'public')))
 
-app.put('/pokemon', (req, res) => {
-
+app.put('https://pokeapi.co/api/v2/pokemon', (req, res) => {
+  const pokemon_name = pokemon.find({name: req.body.referer})
+  res(pokemon_name);
+  res.status(404).send(err);
 })
 
 app.listen(port, () => {
